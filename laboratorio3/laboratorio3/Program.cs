30ff82b3146ef576cff2e88efcd0b223631e0c99
rodol <rodol@RODOLFO>
2024-09-03 18:52:06 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio3/laboratorio3/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int primerNumero, segundoNumero, Suma;
+
+        Console.Write("introduce el primer numero ");
+        primerNumero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("introduce el segundo numero ");
+        segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+        Suma=primerNumero+segundoNumero;
+
+        Console.WriteLine("la suma de {0} y {1} es {2}",primerNumero,segundoNumero,Suma);
+    }
+}
